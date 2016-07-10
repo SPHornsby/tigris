43dece66ad7a5d0ace09d660e2dc692e4a86a2c6
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-10 12:24:23 -0700

    Adds cart div and button. #2

--- public/app.js
+++ public/app.js
@@ -1,19 +1,15 @@
 var submitSearch = function(term) {
-  console.log(`Searching for ${term}`);
-  var query = term;
-  console.log(query);
   var xhr = new XMLHttpRequest();
   xhr.open("GET", `/search?q=${term}`);
   xhr.send();
   xhr.addEventListener("load", function() {
     if (xhr.responseText) {
       var results = JSON.parse(xhr.responseText);
-      console.log(results);
       displayResults(results);
     } else {
       console.log("No response");
     }
-  })
+  });
 };
 var displayResults = function(results) {
   var target = $("#resultList");
@@ -21,6 +17,7 @@ var displayResults = function(results) {
   results.forEach(function(item) {
     makeItemBar(item, target);
   });
+  swap($(".results"));
 };
 var makeItemBar = function(item, target) {
   var itemBar = $("<div>").addClass("item-bar col-md-11");
@@ -37,15 +34,25 @@ var makeItemBar = function(item, target) {
   $(itemBar).append(itemImage, itemDetails);
   $(target).append(itemBar);
 };
-
-$(".search-button").on("click", function(e) {
+var swap = function(next) {
+  var current = $(".current");
+  $(current).addClass("hidden");
+  $(next).removeClass("hidden")
+    .addClass("current");
+}
+$(".search-button").on("click", function() {
   var input = $("#search-input").val();
   submitSearch(input);
 });
 $("#search-input").on("keypress", function(e) {
-  console.log(e);
   if (e.keyCode === 13) {
     var input = $("#search-input").val();
     submitSearch(input);
   }
+});
+$("#cart-button").on("click", function() {
+  swap($(".shopping-cart"));
+});
+$("#home-button").on("click", function() {
+  swap($(".home"));
 });
