e51a299a54dd2a77939e07e38717f8d312e6ceee
Sean Hornsby <sean.hornsby@gmail.com>
2016-07-10 15:35:18 -0700

    In cases where the cookie outlasts the session, create a new user with the existing cookie.

--- routes/checkCookie.js
+++ routes/checkCookie.js
@@ -4,6 +4,7 @@ var users = require("../data/users.js").data;
 
 var checkCookie = function(req, res, next) {
   if (req.cookies.sessionID) {
+    checkForUser(cookie);
     next();
   } else {
     var cookie = Date.now();
@@ -13,8 +14,16 @@ var checkCookie = function(req, res, next) {
   }
 };
 
-var createUser = function(cookie){
-  users.push({user: cookie, cart:[]});
+var createUser = function(id){
+  users.push({user: id, cart:[]});
 };
-
+var checkForUser = function(id) {
+  var user = users.filter(function(user) {
+    return user.user == id;
+  });
+  if (user.length === 0) {
+    console.log("User not found, creating");
+    createUser(id);
+  };
+};
 module.exports = checkCookie;
